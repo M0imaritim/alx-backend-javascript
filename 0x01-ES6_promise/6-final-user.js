1db630e183d98fc0664fd60313bf0145580a236d
Langat Moimaritm <moimaritim@gmail.com>
2025-04-15 18:10:27 +0000

    fixed import error

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,20 +1,21 @@
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]).then((results) => results.map((result) => {
-    if (result.status === 'fulfilled') {
-      return {
-        status: result.status,
-        value: result.value,
-      };
-    }
-    return {
-      status: result.status,
-      value: result.reason,
-    };
-  }));
+export default async function handleProfileSignup(
+  firstName,
+  lastName,
+  fileName,
+) {
+  const res = [];
+  try {
+    const user = await signUpUser(firstName, lastName);
+    res.push({ status: 'fulfilled', value: user });
+    await uploadPhoto(fileName);
+  } catch (err) {
+    res.push({
+      status: 'rejected',
+      value: `Error: ${fileName} cannot be processed`,
+    });
+  }
+  return res;
 }
