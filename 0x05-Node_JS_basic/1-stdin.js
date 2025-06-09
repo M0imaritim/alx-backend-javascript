2514fdc4fefc04c6d8443c8ae20f604bb5f81756
Moimaritim <moimaritim@gmail.com>
2025-06-09 19:13:41 +0300

    Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,10 @@
-process.stdout.write("Welcome to ALX, what is your name?\n");
+process.stdout.write('Welcome to ALX, what is your name?\n');
 
-process.stdin.on("data", (data) => {
+process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   console.log(`Your name is: ${name}`);
 });
 
-process.stdin.on("end", () => {
-  console.log("This important software is now closing");
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
 });
