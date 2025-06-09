74b569d2c4b3be5005ebbfe3fb5c1cc35ef5fc5f
Moimaritim <moimaritim@gmail.com>
2025-06-09 19:23:45 +0300

    lint fix

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,8 +1,10 @@
 process.stdout.write('Welcome to ALX, what is your name?\n');
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  console.log(`Your name is: ${name}`);
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
-process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+process.on('exit' () => {
+  process.stdout.write('This important software is now closing\n');
 });
