3959cc74f143ef9d7967409bf9dfd5b29cff78b0
Moimaritim <moimaritim@gmail.com>
2025-06-11 00:28:14 +0300

    server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -6,8 +6,6 @@ const app = http.createServer((req, res) => {
   res.end('Hello ALX!');
 });
 
-app.listen(1245, () => {
-  console.log('Server running on http://localhost:1245/');
-});
+app.listen(1245);
 
 module.exports = app;
