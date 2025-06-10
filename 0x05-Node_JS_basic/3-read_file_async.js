11c827a0b76caeba78a4d02f8ad3c74cce6ad811
Moimaritim <moimaritim@gmail.com>
2025-06-11 00:09:28 +0300

    read file asynch

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -37,7 +37,7 @@ function countStudents(path) {
           console.log(
             `Number of students in ${field}: ${
               studentNames.length
-            }. List: ${studentNames.join(', ')}`
+            }. List: ${studentNames.join(', ')}`,
           );
         });
 
